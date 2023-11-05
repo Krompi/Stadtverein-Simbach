5cc7d7bb629ea6c0d4869c1a9c66f637e2e899cc
Krompi <krompi@gmail.com>
2023-11-05 20:02:16 +0100

    Seitenanzeige angepasst

---
+++ app/Http/Controllers/PageDisplayController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
+use App\Repositories\PageRepository;
+
+class PageDisplayController extends Controller
+{
+    public function show(string $slug, PageRepository $pageRepository): View
+    {
+        $page = $pageRepository->forSlug($slug);
+ 
+        if (!$page) {
+            abort(404);
+        }
+ 
+        return view('site.page', ['item' => $page]);
+    }
+}
