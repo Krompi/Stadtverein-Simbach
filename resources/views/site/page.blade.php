a3e357a3303839a460a227b04889a79cdde93b35
Krompi <krompi@gmail.com>
2023-11-05 21:13:19 +0100

    Page angepasst

--- resources/views/site/page.blade.php
+++ resources/views/site/page.blade.php
@@ -8,9 +8,21 @@
 
 <body>
     <x-menu/> 
-    <div class="mx-auto max-w-2xl">
-        {!! $item->renderBlocks() !!}
-    </div>
+    <section class="p-6 bg-gray-50 rounded-sm ">
+        <div class="prose prose-xl prose-gray max-w-full">
+            <h1>{!! $item->title !!}</h1>
+
+            @if ($item->hasImage('cover'))
+                <img class="float-left rounded-sm w-1/2 my-2 mr-6" src="{{$item->image('cover')}}">
+            @endif
+
+            @if (trim($item->description) != '')
+                <p class="lead">{!! $item->description !!}</p>
+            @endif
+
+            {!! $item->renderBlocks() !!}
+        </div>
+    </section>
 </body>
 
 </html>
