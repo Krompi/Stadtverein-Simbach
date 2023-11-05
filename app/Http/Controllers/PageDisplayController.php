2c268457ecc16d036f7dea4e76792efce7d553cd
Krompi <krompi@gmail.com>
2023-11-05 20:12:09 +0100

    Startseite eingerichtet

--- app/Http/Controllers/PageDisplayController.php
+++ app/Http/Controllers/PageDisplayController.php
@@ -5,9 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\View\View;
 use App\Repositories\PageRepository;
+use A17\Twill\Facades\TwillAppSettings;
 
 class PageDisplayController extends Controller
 {
+    public function home(): View
+    {
+        if (TwillAppSettings::get('homepage.homepage.page')->isNotEmpty()) {
+            /** @var \App\Models\Page $frontPage */
+            $frontPage = TwillAppSettings::get('homepage.homepage.page')->first();
+ 
+            if ($frontPage->published) {
+                return view('site.page', ['item' => $frontPage]);
+            }
+        }
+ 
+        abort(404);
+    }
+    
     public function show(string $slug, PageRepository $pageRepository): View
     {
         $page = $pageRepository->forSlug($slug);
